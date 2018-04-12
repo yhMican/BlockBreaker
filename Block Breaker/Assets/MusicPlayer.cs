c5babf68e914ab113994a33f725aed535213d738
Yoshitaka Hiramatsu <yoshitaka.too@gmail.com>
2018-04-11 18:22:37 -0700

    up to lec68

---
+++ Block Breaker/Assets/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	static MusicPlayer instance = null;
+
+	// Use this for initialization
+	void Start () {
+		if (instance != null) {
+			Destroy (gameObject);
+			print ("Duplicate music player self-destructing!");
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject); //gameObject is Music Player
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
