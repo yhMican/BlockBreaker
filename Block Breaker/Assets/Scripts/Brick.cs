fd57643f2880a7bdd3b84afb4caf1826515f6ba7
Yoshitaka Hiramatsu <yoshitaka.too@gmail.com>
2018-04-15 15:09:26 -0700

    up to lec 92

--- Block Breaker/Assets/Scripts/Brick.cs
+++ Block Breaker/Assets/Scripts/Brick.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour {
 
-	public int maxHits;
+	public AudioClip crack;
+	public Sprite[] hitSprites;
+	public static int breakableCount = 0;
+
 	private int timesHit;
+	private LevelManager levelManager;
+	private bool isBreakable;
 
 	// Use this for initialization
 	void Start () {
+		isBreakable = (this.tag == "Breakable");
+		// Keep track of breakable bricks
+		if (isBreakable) {
+			++breakableCount;
+			print (breakableCount); // Static var is not exposed in the inspector.
+		}
+
 		timesHit = 0;
+		levelManager = GameObject.FindObjectOfType<LevelManager> ();
 	}
 	
 	// Update is called once per frame
@@ -18,6 +31,33 @@ public class Brick : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D (Collision2D col) {
+		AudioSource.PlayClipAtPoint (crack, transform.position);
+
+		if (isBreakable) {
+			HandleHits (); 
+		}
+	}
+
+	void HandleHits () {
 		++timesHit;
+		int maxHits = hitSprites.Length + 1;
+		if (timesHit >= maxHits) {
+			--breakableCount;
+			levelManager.BrickDestroyed ();
+			Destroy (gameObject);
+		} else {
+			LoadSprites ();
+		}
+	}
+
+	void LoadSprites () {
+		int spriteIndex = timesHit - 1;
+		if (hitSprites [spriteIndex]) {
+			this.GetComponent<SpriteRenderer> ().sprite = hitSprites [spriteIndex];
+		}
+	}
+	// TODO Remove this method once we can actually win!
+	void SimulateWin () {
+		levelManager.LoadNextLevel ();
 	}
 }
