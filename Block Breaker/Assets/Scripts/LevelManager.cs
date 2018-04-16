2943cee9d0ea5d3ea05fc225ac6bd70bd216bc85
Yoshitaka Hiramatsu <yoshitaka.too@gmail.com>
2018-04-15 17:46:04 -0700

    all lectures completed
    boing sound is mulworking but left it as it is for now

--- Block Breaker/Assets/Scripts/LevelManager.cs
+++ Block Breaker/Assets/Scripts/LevelManager.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour {
+	
 
 	public void LoadLevel (string name) {
 		Debug.Log ("Level load requested for: " + name);
+		Brick.breakableCount = 0;
 		Application.LoadLevel (name);
 	}
 
@@ -15,7 +17,9 @@ public class LevelManager : MonoBehaviour {
 	}
 
 	public void LoadNextLevel () {
+		Brick.breakableCount = 0;
 		Application.LoadLevel (Application.loadedLevel + 1);
+
 	}
 
 	public void BrickDestroyed () {
